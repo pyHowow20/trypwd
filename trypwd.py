0e5cd3c10b263d11bcdab6d92a8f2e3aa96d770c
pyHoward <wang.howard@gmail.com>
2020-08-24 11:23:56 +0800

    Changed to while count > 0

--- trypwd.py
+++ trypwd.py
@@ -1,7 +1,8 @@
+# 密碼重試程式
 password = 'a123456'
 print ('最多可輸入3次密碼錯誤')
-count = 3
-while True:
+count = 3 #剩餘機會
+while count > 0:
 	pwd = input ('請輸入密碼: ')
 	if pwd == password :
 		print ('登入成功!')
@@ -9,5 +10,3 @@ while True:
 	else:
 		count = count - 1
 		print ('輸入錯誤, 你還有', count, '次機會')
-		if count == 0:
-			break
